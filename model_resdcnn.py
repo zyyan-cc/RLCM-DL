a0b7027ea82c73312d823c957764075fdd08570c
zyyan-cc <zyyan23@mails.jlu.edu.cn>
2025-11-04 20:43:32 +0800

    Update model_resdcnn.py

--- model_resdcnn.py
+++ model_resdcnn.py
@@ -1,78 +1,113 @@
-from torch import nn as nn
 import torch
+import torch.nn as nn
+import numpy as np
+import pandas as pd
 
 
-class ResDCNN(nn.Module):
-    def __init__(self, model_arch=[64, 64, 128, 128, 256]):
-        super(ResDCNN, self).__init__()
-
-        downsample1 = nn.Sequential(
-                nn.Conv1d(64, 128, kernel_size=3, stride=2, bias=False, padding=1),
-                nn.BatchNorm1d(128))
-        
-        downsample2 = nn.Sequential(
-                nn.Conv1d(128, 256, kernel_size=1, stride=1, bias=False),
-                nn.BatchNorm1d(256))
-        
-        self.fc = nn.Linear(model_arch[-1], 1)
-
-        self.conv1 = nn.Sequential(
-            nn.Conv1d(1, 64, kernel_size=3, stride=1,
-                    padding=1, dilation=1, groups=1, bias=False),
-            nn.BatchNorm1d(64),
-            nn.ReLU())
-        
-        self.conv2 = Bottleneck(64, 64)
-        self.conv3 = Bottleneck(64, 128, stride=2, downsample=downsample1)
-        self.conv4 = Bottleneck(128, 128)
-        self.conv5 = Bottleneck(128, 256, stride=1, downsample=downsample2)
-
-        self.avg = nn.AdaptiveAvgPool1d(1)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        x = self.conv5(x)
-        
-        out = self.avg(x)
-
-        out = out.view(out.size(0), -1)
-        out = self.fc(out)
-
-        return out
-    
-    
-class Bottleneck(nn.Module):
-    
-    def __init__(self, in_channel, out_channel, stride=1, downsample=None):
-        super(Bottleneck, self).__init__()
+class BasicBlock(nn.Module):
+    expansion = 1
  
+    def __init__(self, in_channel, out_channel, stride=1, downsample=None, **kwargs):
+        super(BasicBlock, self).__init__()
+
         self.conv1 = nn.Conv1d(in_channels=in_channel, out_channels=out_channel,
-                               kernel_size=3, stride=stride, bias=False, padding=1)
+                               kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm1d(out_channel)
         self.relu = nn.ReLU()
-        
+        self.conv2 = nn.Conv1d(in_channels=out_channel, out_channels=out_channel,
+                               kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm1d(out_channel)
         self.downsample = downsample
 
     def forward(self, x):
-
         identity = x
-
         if self.downsample is not None:
             identity = self.downsample(x)
  
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
-        out = out + identity
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        out += identity
+        out = self.relu(out)
  
         return out
 
-if __name__ == "__main__":
-    input_tensor = torch.rand(size=(1, 1, 8))
-    net = ResDCNN()
-    out = net(input_tensor)
 
-    print(out.shape)
+class ResDCNN(nn.Module):
+    def __init__(self,
+                 block=BasicBlock,
+                 blocks_num=[1, 1, 1],
+                 num_classes=1,
+                 include_top=True,
+                 groups=1,
+                 width_per_group=64):
+        super(ResDCNN, self).__init__()
+        self.include_top = include_top
+        self.in_channel = 64
+ 
+        self.groups = groups
+        self.width_per_group = width_per_group
+ 
+        self.conv1 = nn.Conv1d(1, self.in_channel, kernel_size=3, stride=1,
+                               padding=1, bias=False)
+        self.bn1 = nn.BatchNorm1d(self.in_channel)
+
+        self.layer1 = self._make_layer(block, 64, blocks_num[0])
+        self.layer2 = self._make_layer(block, 128, blocks_num[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, blocks_num[2], stride=1)
+
+        if self.include_top:
+            self.avgpool = nn.AdaptiveAvgPool1d(1)
+            self.fc = nn.Linear(256 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv1d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+
+
+    def _make_layer(self, block, channel, block_num, stride=1):
+        downsample = None
+
+        if stride != 1 or self.in_channel != channel * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv1d(self.in_channel, channel * block.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm1d(channel * block.expansion))
+ 
+        layers = []
+        layers.append(block(self.in_channel,
+                            channel,
+                            downsample=downsample,
+                            stride=stride,
+                            groups=self.groups,
+                            width_per_group=self.width_per_group))
+        self.in_channel = channel * block.expansion
+ 
+        for _ in range(1, block_num):
+            layers.append(block(self.in_channel,
+                                channel,
+                                groups=self.groups,
+                                width_per_group=self.width_per_group))
+
+        return nn.Sequential(*layers)
+ 
+
+    def forward(self, x):
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        # print(x.shape)
+        if self.include_top:
+            x = self.avgpool(x)
+            x = x.view(x.size(0), -1)
+            x = self.fc(x)
+
+        return x
+
